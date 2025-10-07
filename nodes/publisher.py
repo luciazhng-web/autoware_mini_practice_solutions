3488e8b103802e9f2c3a593aa3d40069e9fdbdaf
Lucia <luciazhng@gmail.com>
2025-10-08 03:14:10 +0800

    Verified reusing nodes using different parameters with launch file

--- nodes/publisher.py
+++ nodes/publisher.py
@@ -4,9 +4,13 @@ import rospy
 from std_msgs.msg import String
 
 rospy.init_node('publisher')
-rate = rospy.Rate(2)
+
+freq = rospy.get_param('~rate', 5) #add for 7 - Reusing nodes
+message = rospy.get_param('~message', 'Hello World!') #add for 6 - Parameters in launch files and nodes
+rate = rospy.Rate(freq)
 pub = rospy.Publisher('/message', String, queue_size=10)
 
+
 while not rospy.is_shutdown():
-    pub.publish("Hello world!")
+    pub.publish(message) #replacing the hardcoded message, for 6 - Parameters in launch files and nodes
     rate.sleep()
