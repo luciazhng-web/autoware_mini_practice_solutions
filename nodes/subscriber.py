fe8040985409d48837464ededed36b193a700dd0
Lucia <luciazhng@gmail.com>
2025-10-08 02:06:48 +0800

    Verified rospack and successfully ran nodes using rosrun, demonstrating full package executability.

---
+++ nodes/subscriber.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+def callback(msg):
+    print(msg.data)
+
+rospy.init_node('subscriber')
+rospy.Subscriber("/message", String, callback)
+rospy.spin()
